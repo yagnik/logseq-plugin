21a6a2134a32607d5215affce98ac400edc5a6f3
Yagnik <yagnik@users.noreply.github.com>
2022-10-13 13:50:35 +0000

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # logseq-plugin
+
+Personal plugins to manage logseq
+
+## Plugins
+- Autolog
+  - Pull blocks from journal and put them in one place
+  - Use it to track updates on thoughts
+  - Backlink is not useful cause it mixes pages and journals
+  - #idea In the future backlinks could be segregated between journals and pages
+- Validator
+  - Validate data of page property
+  - Ensure that scripts using properties have correct data
+- Contact reminder
+  - Use page properties + journal data to remind when to contact
+  - #idea could automatically understand people association and suggest
--- src/lib/inifinityAvoidance.ts
+++ src/lib/inifinityAvoidance.ts
@@ -1,15 +1,21 @@
+// Add an updated at field to ensure the renderer doesn't do infinite loop
+// https://github.com/logseq/logseq/issues/6937
+
+const SLOT_KEY = "_slot"
+const UPDATED_KEY = "_updatedAt"
+
 export async function avoidRender(
   slot: string,
   uuid: string,
   duration: number
 ) {
-  let currentSlot = await logseq.Editor.getBlockProperty(uuid, "slot");
-  let lastUpdatedAt = await logseq.Editor.getBlockProperty(uuid, "updatedAt");
+  let currentSlot: String = await logseq.Editor.getBlockProperty(uuid, SLOT_KEY);
+  let lastUpdatedAt: number = await logseq.Editor.getBlockProperty(uuid, UPDATED_KEY);
   let currentTime = Date.now();
   let result = currentTime - lastUpdatedAt < duration && slot !== currentSlot;
   if (!result) {
-    await logseq.Editor.upsertBlockProperty(uuid, "updatedAt", currentTime);
-    await logseq.Editor.upsertBlockProperty(uuid, "slot", slot);
+    await logseq.Editor.upsertBlockProperty(uuid, UPDATED_KEY, currentTime);
+    await logseq.Editor.upsertBlockProperty(uuid, SLOT_KEY, slot);
   }
   return result;
 }
